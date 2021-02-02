a2bc88d8a35112d9da3d88411c3b261182331062
Muyuan Cao <cmyhamster@gmail.com>
2021-02-02 13:29:39 -0500

    hi

--- part2/part2-parallel-opt.c
+++ part2/part2-parallel-opt.c
@@ -18,47 +18,92 @@
 #include "data.h"
 #include "time_util.h"
 
+int32_t threads;
+course_record *all_courses;
+int32_t chunk_size;
+
 //TODO: parallelize the code and optimize performance
 
-//typedef struct _course_record_opt {
-//	char name[8];
-//	grade_record *grades;// The grades of all the students that have taken this course
-//	int grades_count;// Length of the 'grades' array
-//	double average;// Historic average grade; must be (re)computed by this program
-//	char Padding[32];
-//} course_record_opt;
-
-// Compute the historic average grade for a given course. Updates the average value in the record
-void compute_average(course_record *course) {
-	assert(course != NULL);
-	assert(course->grades != NULL);
-
-	double average = 0.0;
-	int course_count = course->grades_count;
-
-	grade_record *course_grades = course->grades;
-
-	for (int i = 0; i < course_count; i++) {
-		average += (double) (course_grades[i].grade);
+void compute_average_sharded(void *val){
+	int chunk_offset = (*(int*)val) *chunk_size;
+	for (int i = chunk_offset; i< chunk_size + chunk_offset; i++ ){
+		compute_average(i);
 	}
-	course->average = average/course_count;
-	pthread_exit(NULL);
 }
 
+void compute_average(int nth_course)
+{
+	assert(all_courses[nth_course] != NULL);
+	assert(all_courses[nth_course]->grades != NULL);
+
+	all_courses[nth_course]->average = 0.0;
+	for (int i = 0; i < course->grades_count; i++) {
+		all_courses[nth_course]->average += all_courses[nth_course]->grades[i].grade;
+	}
+	all_courses[nth_course]->average /= all_courses[nth_course]->grades_count;
+
+}
+
+//void compute_average(course_record *course) {
+//	assert(course != NULL);
+//	assert(course->grades != NULL);
+//
+//	pthread_t tpool[threads];
+//	int idx[threads];
+//	int rc;
+//
+//	for (int i = 0; i < threads; i++){
+//		idx[i] = i;
+//		rc = pthread_create(&tpool[i], NULL, compute_average_sharded, (void*)&idx[i]);
+//
+//	}
+//
+//	if (rc)
+//	{
+//		fprintf(stderr, "return code from pthread_create() is %d\n", rc);
+//		exit(-1);
+//	}
+//
+//	for (int i = 0; i < threads; ++i) {
+//		rc = pthread_join(tpool[i], NULL);
+//		if (rc) exit(-1);
+//	}
+//
+//	return NULL;
+//}
+
+
+
+// Compute the historic average grade for a given course. Updates the average value in the record
+//void compute_average(course_record *course) {
+//	assert(course != NULL);
+//	assert(course->grades != NULL);
+//
+//	double average = 0.0;
+//	int course_count = course->grades_count;
+//
+//	//grade_record *course_grades = course->grades;
+//	for (int i = 0; i < course_count; i++) {
+//		average += course->grades[i].grade;
+//	}
+//	course->average = average/course_count;
+//	pthread_exit(NULL);
+//}
 
 
 // Compute the historic average grades for all the courses
 void compute_averages(course_record *courses, int courses_count) {
 	assert(courses != NULL);
 
-	pthread_t thread_id[courses_count];
+	int threads = ((int) courses_count/2 )+ (courses%2);
+	pthread_t thread_id[threads];
 	int ret, rc;
 
 
-	for (int i = 0; i < courses_count; i++) {
+	for (int i = 0; i < threads; i++) {
 		//multi thread for each course
 
-		ret = pthread_create(&thread_id[i],NULL, (void *)compute_average, (void *)&(courses[i]) );//input: &id, Null,
+		ret = pthread_create(&thread_id[i],NULL, (void *)compute_average, (void *)&(thread_id[i]) );//input: &id, Null,
 		if (ret != 0){
 			printf("create pthread error!\n");
 			return;
@@ -78,22 +123,24 @@ void compute_averages(course_record *courses, int courses_count) {
 
 int main(int argc, char *argv[])
 {
-	course_record *courses;
+	//course_record *courses;
 	int courses_count;
+	threads = courses_count;
+	chunk_size = 2;
 	// Load data from file; "part2data" is the default file path if not specified
-	if (load_data((argc > 1) ? argv[1] : "part2data", &courses, &courses_count) < 0) return 1;
+	if (load_data((argc > 1) ? argv[1] : "part2data", &all_courses, &courses_count) < 0) return 1;
 
 	struct timespec start, end;
 	clock_gettime(CLOCK_MONOTONIC, &start);
-	compute_averages(courses, courses_count);
+	compute_averages(all_courses, courses_count);
 	clock_gettime(CLOCK_MONOTONIC, &end);
 
 	for (int i = 0; i < courses_count; i++) {
-		printf("%s: %f\n", courses[i].name, courses[i].average);
+		printf("%s: %f\n", all_courses[i].name, all_courses[i].average);
 	}
 
 	printf("%f\n", timespec_to_msec(difftimespec(end, start)));
 
-	free_data(courses, courses_count);
+	free_data(all_courses, all_courses);
 	return 0;
 }
